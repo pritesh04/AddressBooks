9a4f9f75a25e88be7d1853fad2fc287bda529ec0
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-09-06 09:47:16 +0530

    [Pritesh] Added contacts using stream Api

---
+++ src/com/bridgelab/addressbook/AddressBookCollections.java
@@ -0,0 +1,127 @@
+package com.bridgelab.addressbook;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class AddressBookCollections {
+
+	public int i = 0;
+	public List<CollegeAddressBook> list = new ArrayList<CollegeAddressBook>();
+
+	public void searchCity() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter City: ");
+		String city = sc.nextLine();
+
+		for (int i = 0; i < list.size(); i++) {
+
+			if (city.equals(list.get(i).city)) {
+				System.out.println(list.get(i).firstName);
+			}
+		}
+	}
+
+	public void searchState() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter state: ");
+		String state = sc.nextLine();
+
+		for (int i = 0; i < list.size(); i++) {
+			if (state.equals(list.get(i).state)) {
+				System.out.println(list.get(i).firstName);
+			}
+		}
+
+	}
+
+	public void countPersons() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter State Name");
+		String state = sc.nextLine();
+
+		list.stream().filter(n -> n.equals(state)).forEach(System.out::println);
+//		int count = 0;
+//		for (int i = 0; i < list.size(); i++) {
+//			if (state.equals(list.get(i).state)) {
+//				count++;
+//
+//			}
+//
+//		}
+//		System.out.println(count);
+	}
+
+	
+	public boolean checkDuplicate(String firstname) {
+
+		for (int i = 0; i < list.size(); i++) {
+
+//			if (firstname.equals(list.get(i).firstName)) {
+//				return false;
+//			}
+			if (list.stream().anyMatch(n -> n.equals(firstname))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void addContact() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of entries: ");
+		int record = sc.nextInt();
+		for (int i = 0; i < record; i++) {
+			System.out.println("Enter First Name : ");
+			String firstname = sc.next();
+			System.out.println("Enter Last Name : ");
+			String lastname = sc.next();
+			System.out.println("Enter Address : ");
+			String address = sc.next();
+			System.out.println("Enter City : ");
+			String city = sc.next();
+			System.out.println("Enter State : ");
+			String state = sc.next();
+			System.out.println("Enter ZipCode : ");
+			String zipcode = sc.next();
+			System.out.println("Enter Phone Number : ");
+			String phonenum = sc.next();
+			System.out.println("Enter Email id : ");
+			String email = sc.next();
+
+			if (this.checkDuplicate(firstname)) {
+
+				CollegeAddressBook addressBookCollections = new CollegeAddressBook(firstname, lastname, address, city,
+						state, zipcode, phonenum, email);
+				list.add(addressBookCollections);
+				System.out.println("Contact added ");
+			} else {
+				System.out.println("Duplicate found");
+			}
+		}
+	}
+
+	public void showContact() {
+		list.stream().forEach(System.out::println);
+//		Iterator<ContactAddressBook> itr = list.iterator();
+//		while (itr.hasNext()) {
+//			System.out.println(itr.next());
+//		}
+	}
+
+	public void deleteContact() {
+		this.showContact();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Name to delete ");
+		String name = sc.next();
+		for (int i = 0; i < list.size(); i++) {
+			if (name.equals(list.get(i).firstName)) {
+				list.remove(i);
+			}
+		}
+	}
+
+}
---
+++ src/com/bridgelab/addressbook/AddressBookRunner.java
@@ -0,0 +1,46 @@
+package com.bridgelab.addressbook;
+
+import java.util.Scanner;
+
+public class AddressBookRunner {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Address Book Program");
+		AddressBookCollections book = new AddressBookCollections();
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.println("Enter Choice: ");
+			System.out.println(
+					"1. Add Contact , 2. Edit Contact , 3. Show Contacts , 4. Search By City  , 5. Search By State ,  6. View Persons  ,7. Count Persons,   8. Exit");
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				book.addContact();
+				break;
+			case 2:
+				book.deleteContact();
+				break;
+			case 3:
+				book.showContact();
+				break;
+			case 4:
+				book.searchCity();
+				break;
+			case 5:
+				book.searchState();
+				break;
+			case 6:
+				//book.viewPersons();
+				break;
+			case 7:
+				book.countPersons();
+				break;
+
+			default:
+				System.out.println("Enter Correct Choice");
+				break;
+			}
+		}
+
+	}
+}
